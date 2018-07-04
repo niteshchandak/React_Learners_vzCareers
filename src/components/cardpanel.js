1d311405fa888940aa69c1456b3918aaf1b0458e
nitesh <nitesh12041988@gmail.com>
2018-07-04 07:33:38 +0000

    dynamic widgets added

--- src/components/cardpanel.js
+++ src/components/cardpanel.js
@@ -15,16 +15,11 @@ class CardPanel extends Component {
       })
   }
     render() {
-      console.log(this.state);
-      const numbers = this.state;
-    var arr = [];
-    Object.keys(numbers).forEach(function(key) {
-      arr.push(numbers[key]);
-    });                 
+                      
     return (
       <div className="container-fluid margin-top-xl jobsearchBox">
       <div className="row">
-      {arr.map(item => <MyAppChild designation={item.designation} Experience={item.Experience} location={item.job_location} skills={item.technologies}  postedDate={item.posting_date}/>)}
+      {this.state.persons.map(person => <MyAppChild designation={person.designation} hiringmanager={person.hiringmanager} Experience={person.experience} location={person.joblocation} skills={person.technologies}  postedDate={person.postingdate}/>)}
       </div>
       </div>);
     }
@@ -39,10 +34,10 @@ class MyAppChild extends React.Component {
                 <h3 class="panel-title"><b>{this.props.designation}</b></h3>
               </div>
               <div class="panel-body">
-                <p><b>Verizon Data Service</b></p>
+                <p><b>Hiring Manager: <i>{this.props.hiringmanager}</i></b></p>
                 <ul>
-                  <li><span class="glyphicon glyphicon-briefcase"></span> {this.props.Experience}</li>
-                  <li><span class="glyphicon glyphicon-pushpin"></span> {this.props.location}</li>
+                  <li>Experience <span class="glyphicon glyphicon-briefcase"></span> {this.props.Experience}</li>
+                  <li>Location<span class="glyphicon glyphicon-pushpin"></span> {this.props.location}</li>
                 </ul>
                 <p>Key Skills: <i>{this.props.skills}</i></p>
               </div>
